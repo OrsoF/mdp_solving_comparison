ba3cbf4238432bd8081e9730c19c3f5ab1f097d0
Orso Forghieri <orso.forghieri@gmail.com>
2022-11-28 14:42:09 +0100

    Update

--- util.py
+++ util.py
@@ -4,11 +4,6 @@ import os
 import pickle
 import pandas as pd
 
-# import solvers
-# import environments
-# solv_init = solvers.init
-# envs_init = environments.init
-
 def gurobi_license():
     grb.Env('gurobi.log')
     f = open("gurobi.log", "r")
@@ -35,29 +30,3 @@ def pickle_open(path):
     with open(path, 'rb') as handle:
         file = pickle.load(handle)
     return file
-
-results_files = os.listdir(os.path.join(os.getcwd(), 'results'))
-
-def show_results():
-    table_dict_build, table_dict_run = {}, {}
-    table_dict_build['Env'], table_dict_run['Env'] = envs_init.envs_list, envs_init.envs_list
-    # Res columns
-    res = os.listdir(os.path.join(os.getcwd(), 'results'))
-    for elem in res:
-        file_path = os.path.join(os.getcwd(), 'results', elem)
-        res = pickle_open(file_path)
-        if 'build' in elem:
-            table_dict_build[elem[8:-13]] = res
-        else:
-            table_dict_run[elem[8:-8]] = res
-
-    res_df_build = pd.DataFrame(table_dict_build)
-    res_df_run = pd.DataFrame(table_dict_run)
-    print()
-    print(res_df_build.style.to_latex())
-    print()
-    print(res_df_run.style.to_latex())
-    print()
-    print(res_df_build)
-    print()
-    print(res_df_run)
